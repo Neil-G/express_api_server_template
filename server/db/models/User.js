03bc9053ee846b49c04525208631b104e1e36522
neil <ngneilgordon@gmail.com>
2019-09-02 16:25:48 -0400

    remove unused user model fields

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
-const { Schema } = mongoose
-
 
 module.exports =  {
   name: 'User',
   config: {
-    firstName:  String,
-    lastName:   String,
-    userName:   { type: String,   unique: true },
+    // firstName:  String,
+    // lastName:   String,
+    // userName:   { type: String,   unique: true },
     email:      { type: String,   required: true,   unique: true },
     password:   { type: String,   required: true },
     archived:   { type: Boolean,  default: false },
