3d08519d057ad12e57ac96dffc8d0551de55b4ad
neil <ngneilgordon@gmail.com>
2019-09-01 20:21:14 -0400

    add handlebars,  add homepage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config')
@@ -11,6 +12,9 @@ const GraphQLSchema = require('./src/graphql')
 // static folder
 app.use(express.static('public'))
 
+// template engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 // format request info
 app.set('json spaces', 2)
@@ -18,10 +22,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// public pages
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
 // auth routes
 app.use('/auth', authRoutes)
-
 
 // graphql endpoint
 app.use('/graphql', graphqlHTTP({
@@ -29,7 +36,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
   pretty: true
 }))
-
 
 // start up server
 app.listen(5678, () => {
