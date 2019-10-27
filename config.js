2e80f4cfa3d5aa622e20e28dafe3e39a6939fb68
neil <ngneilgordon@gmail.com>
2019-10-27 14:23:56 -0400

    add appRouteUrl to config

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 module.exports = {
   development: {
-    port: 1000
+    port: 1000,
+    appRootUrl: 'http://localhost:3000'
+  },
+  production: {
+    appRootUrl: '/app'
+  },
+  testing: {
+
   }
 }
