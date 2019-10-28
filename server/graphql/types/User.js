07741fd9ccfff2755f9f95fa7d7a739cd4294d80
neil <ngneilgordon@gmail.com>
2019-10-27 20:23:01 -0400

    apply utils to user type

--- server/graphql/types/User.js
+++ server/graphql/types/User.js
@@ -1,27 +1,30 @@
 const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql')
 const jwt = require('jsonwebtoken')
+const { idField, getTypesFromMongoModel } = require('./utils')
+const { name, config: modelConfig } = require('./../../db/models/User')
+
+/*
+|--------------------------------------------------------------------------
+| Custom Fields
+|--------------------------------------------------------------------------
+*/
+
+const customFields = {
+  token: {
+    type: GraphQLString,
+    resolve: ({ _id }) => jwt.sign({ uid: _id }, 'secret', { expiresIn: '14 days'})
+  }
+}
+
+/*
+|--------------------------------------------------------------------------
+| Custom Fields
+|--------------------------------------------------------------------------
+*/
 
 module.exports = new GraphQLObjectType({
-  name:'user',
+  name: name.toLowerCase(),
   fields:  () => {
-    return {
-      id: {
-        type: GraphQLID,
-        resolve: ({ _id }) => _id
-      },
-      firstName: {
-        type: GraphQLString
-      },
-      lastName: {
-        type: GraphQLString
-      },
-      emailAddress: {
-        type: GraphQLString
-      },
-      token: {
-        type: GraphQLString,
-        resolve: ({ _id }) => jwt.sign({ uid: _id }, 'secret', { expiresIn: '14 days'})
-      }
-    }
+    return { ...idField, ...customFields, ...getTypesFromMongoModel(modelConfig)} 
   }
 })
