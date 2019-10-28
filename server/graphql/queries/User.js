8435a302c3353cf761a1f540bd69b77ecfef15cb
neil <ngneilgordon@gmail.com>
2019-10-27 20:21:19 -0400

    change query arg email to emailAddress

--- server/graphql/queries/User.js
+++ server/graphql/queries/User.js
@@ -11,19 +11,19 @@ exports.getUser = {
       name: 'id',
       type: GraphQLID
     },
-    email: {
-      name: 'email',
+    emailAddress: {
+      name: 'emailAddress',
       type: GraphQLString
     },
   },
-  resolve: async (_, { id, email }) => {
+  resolve: async (_, { id, emailAddress }) => {
 
-    if (!id && !email) {
-      throw Error('Must provide either id or email as a paramter to retrieve a user.')
+    if (!id && !emailAddress) {
+      throw Error('Must provide either id or emailAddress as a paramter to retrieve a user.')
     }
     
     // determine query params by user input
-    const queryArgs = id ? { '_id': id } : { email }
+    const queryArgs = id ? { '_id': id } : { emailAddress }
 
     return await User.findOne(queryArgs).lean()
   }
