44febb165ca649852b71a2c2ec021787640713bf
neil <ngneilgordon@gmail.com>
2019-10-29 14:20:40 -0400

    clean up getUserWithToken gql query

--- server/graphql/queries/User.js
+++ server/graphql/queries/User.js
@@ -5,11 +5,9 @@ const { Models: { User }} = require('./../../db/models')
 // retrieve a user with a token, used on client application init to login
 exports.getUserWithToken = {
   type: UserType,
-  args: {}, // only header token is used
-  resolve: async (_, __, req) => {
-    console.log('in gql query')
+  resolve: async (_, __, { headers: { token }}) => {
     // decode token
-    const decodedToken = jwt.verify(req.headers.token, 'secret')
+    const decodedToken = jwt.verify(token, 'secret')
     return await User.findById(decodedToken.uid).lean()
   }
 }
